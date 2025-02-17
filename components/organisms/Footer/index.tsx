a09fa3f71e91b66cf1b7b7f5156b976e148cb07e
trias <iaszaen@gmail.com>
2025-02-17 17:07:58 +0700

    update lagi coyss

--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -6,16 +6,18 @@ export default function Footer() {
     <footer>
       <div className="container center-text">
         <div className="footer__copyright">
-          <h4>&copy; By Trias & Zulfa </h4>
+        <a href="/#" className="">
+          <h4>&copy; Trias & Zulfa </h4>
+          </a>
         </div>
-        <a href="/#" className="footer__logo">
+        {/* <a href="/#" className="footer__logo">
           Powered by Trias Zaen Mutaqin
-        </a>
-        <div className="footer__socials">
+        </a> */}
+        {/* <div className="footer__socials">
           <a href="https://www.instagram.com/tzm_12/">
             <FiInstagram />
           </a>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
